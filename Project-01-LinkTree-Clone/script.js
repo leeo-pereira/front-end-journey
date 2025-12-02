ac803748a2af30a1d7aee4b4fa4491d3c2aac195
leeopereira <leeo.pereira@gmail.com>
2025-12-02 09:40:01 -0300

    Move theme toggle button to footer and update theme handling in JavaScript

--- Project-01-LinkTree-Clone/script.js
+++ Project-01-LinkTree-Clone/script.js
@@ -1,5 +1,17 @@
 const toggleButton = document.getElementById('theme-toggle');
 const bodyElement = document.body;
+const savedTheme = localStorage.getItem('theme');
+
+if (savedTheme === 'dark') {
+    bodyElement.classList.add('dark-mode');
+}
+
 toggleButton.addEventListener('click', () => {
     bodyElement.classList.toggle('dark-mode');
+
+if (bodyElement.classList.contains('dark-mode')) {
+    localStorage.setItem('theme', 'dark');
+} else {
+    localStorage.setItem('theme', 'light');
+}
 });
